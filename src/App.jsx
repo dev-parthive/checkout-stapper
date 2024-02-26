cadce5756ff639105f55797ad418570392d4139a
dev-parthive <parthivefive@gmail.com>
2024-02-26 23:37:20 +0600

    animation complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
-import CheckoutStepper from './Components/Stepper'
+import CheckoutStepper from "./Components/Stepper";
 function App() {
-const   CHECKOUT_STEPS = [
-  {
-    name: "Customer Info ", 
-    component: () => <div>Provider your contact details </div>
-
-  }, 
-  {
-    name: "Shipping Info ", 
-    component: () => <div> Enter your shipping address  </div>
-  }, 
-  {
-    name: "Payment", 
-    component: () => <div> Complete pyament for your order  </div>
-  }, 
-  {
-    name: "Delivered", 
-    component: () => <div> Your order has been delivered </div>
-  }
-]
+  const CHECKOUT_STEPS = [
+    {
+      name: "Customer Info ",
+      component: () => <div>Provider your contact details </div>,
+    },
+    {
+      name: "Shipping Info ",
+      component: () => <div> Enter your shipping address </div>,
+    },
+    {
+      name: "Payment",
+      component: () => <div> Complete pyament for your order </div>,
+    },
+    {
+      name: "Delivered",
+      component: () => <div> Your order has been delivered </div>,
+    },
+  ];
   return (
-  <>
-        <h3 style={{color:"green",textAlign:"center", fontSize: "3.5rem"}}>Checkout</h3>
+    <>
+      <h3 style={{ color: "green", textAlign: "center", fontSize: "3.5rem" }}>
+        Checkout
+      </h3>
 
-       <CheckoutStepper stepsConfig={CHECKOUT_STEPS}></CheckoutStepper>
-  </>
-  )
+      <CheckoutStepper stepsConfig={CHECKOUT_STEPS}></CheckoutStepper>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/Components/Stepper.jsx
+++ src/Components/Stepper.jsx
@@ -1,23 +1,64 @@
-import React from 'react';
-import '../index.css'
-const stepper = ({stepsConfig = []}) => {
-    if(!stepsConfig.length){
-        return <></>
-    }
-    return (
-        <div className='stepper'>
-           {stepsConfig.map((step, index)=>{
-            return(
-                <div key={step.name} className='step'>
-                <p className='step-number'>{index + 1 }</p>
-                <p className="step-name">{step?.name}</p>
-                
-                </div>
-            )
+import { useState } from "react";
+import "../App.css";
+const stepper = ({ stepsConfig = [] }) => {
+  const [currentStep, setCurrentStep] = useState(1);
+  const [isComplete, setIsComplete] = useState(false);
+  const ActiveComponent = stepsConfig[currentStep - 1]?.component;
+  const handleBtn = () => {
+    setCurrentStep((prevStep) => {
+      if (prevStep === stepsConfig.length) {
+        setIsComplete(true);
+        return prevStep;
+      } else {
+        return prevStep + 1;
+      }
+    });
+  };
+  const calculateProgressBarWidth = () => {
+    return ((currentStep - 1) / (stepsConfig.length - 1)) * 100;
+  };
+  if (!stepsConfig.length) {
+    return <></>;
+  }
+  return (
+    <>
+      <div className="stepper">
+        {stepsConfig.map((step, index) => {
+          return (
+            <div
+              key={step.name}
+              className={`step ${
+                currentStep > index + 1 || isComplete ? "complete" : ""
+              } ${currentStep === index + 1 ? "active" : ""} `}
+            >
+              <p className="step-number">
+                {currentStep > index + 1 || isComplete ? (
+                  <span>&#10003;</span>
+                ) : (
+                  index + 1
+                )}
+              </p>
+              <p className="step-name">{step?.name}</p>
+            </div>
+          );
+        })}
 
-           })}
+        {/* progress bar  */}
+        <div className="progressbar">
+          <div
+            className="progress"
+            style={{ width: `${calculateProgressBarWidth()}%` }}
+          ></div>
         </div>
-    );
+      </div>
+
+      <ActiveComponent className="activeCompo"></ActiveComponent>
+
+      <button className="btn" onClick={handleBtn}>
+        {currentStep > stepsConfig.length - 1 || isComplete ? "Finish" : "Next"}
+      </button>
+    </>
+  );
 };
 
 export default stepper;
